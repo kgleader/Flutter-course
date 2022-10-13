0f8f7425f659a90c8c762ae4f0d928fea42b60a5
FLUTTER <kgleader@gmail.com>
2022-10-13 16:46:19 +0600

    Weather App

--- wather_app3/test/widget_test.dart
+++ wather_app3/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:wather_app3/main.dart';
+import 'package:wather_app3/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
